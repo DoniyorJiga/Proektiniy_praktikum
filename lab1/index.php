4f31b62d6fd275093a87df59fe9c70921f3d3a34
DoniyorJiga <doniyor.jiga@mail.ru>
2019-03-15 16:40:19 +0300

    lab1

---
+++ lab1/index.php
@@ -0,0 +1,10 @@
+﻿<?php 
+$date = $_GET['date']; 
+echo strftime("%A, %d/%m/%Y", strtotime($date)); 
+
+
+$b = strftime("%A"); 
+
+$json_data = array ('day'=>$b); 
+echo json_encode($json_data); 
+?>
